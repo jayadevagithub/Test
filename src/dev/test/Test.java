c67d8e55b51b4be89b1ef3029762959e157241c3
Jayadeva <galijayadeva@gmail.com>
2018-11-01 11:25:37 +0530

    First commit

---
+++ src/dev/test/Test.java
@@ -0,0 +1,8 @@
+package dev.test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("I am going to start my second career from now on ... bless me Vivekananda and Adisankara");
+	}
+
+}
