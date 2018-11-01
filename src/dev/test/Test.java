308cc8bf224eb2a3c27835f1a09f30757e341027
jbabu9 <jbabu9@INCNU3199S57.CSCMWS.CSCMWS.COM>
2018-11-01 11:51:32 +0530

    changed the comment

--- src/dev/test/Test.java
+++ src/dev/test/Test.java
@@ -2,7 +2,7 @@ package dev.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("I am going to start my second career from now on ... bless me Vivekananda and Adisankara");
+		System.out.println("I am going to start my second career from now on ... bless me Vivekananda and Adisankara - changed");
 	}
 
 }
